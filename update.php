a5c39fe96c7c12d278808f2118a6d7a663095d4d
langonginc <langonginc@yeah.net>
2020-05-10 19:59:20 +0800

    Update auto

--- update.php
+++ update.php
@@ -1,8 +1,9 @@
 <?php
+echo "<title>Updating</title>";
 $id=$_GET['id'];
 $val=$_POST['val'];
 $f=fopen("doc/".$id.".md","w");
-fwrite($f,$val);
+fwrite($f,trim($val));
 fclose($f);
 echo "<script>window.location.href=\"/?type=edit&id=".$id."\";</script>";
 ?>
